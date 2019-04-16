2c6baed5b21368c86012e84a99f1c18e262bc3d4
BX <bioxakep@gmail.com>
2019-04-16 22:16:18 +0300

    Server e_print add

--- Server/Server.py
+++ Server/Server.py
@@ -40,6 +40,10 @@ class Command:
 	def to_string(self):
 		return '|'.join([self.dt,self.name,self.scores])
 
+def e_print(text):
+	dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	print('{}: {}'.format(dt, text), end='\n')
+
 @app.errorhandler(404)
 def not_found(error):
     return 'error'
@@ -54,6 +58,7 @@ def reset():
 	global curr_id
 	STATE = WAIT_NAME
 	curr_id = -1
+	e_print('MASTEROPERATOR ONLINE, WAIT RUN GAME FROM MASTER')
 	return 'OK'
 
 @app.route('/startgame', methods = ['GET'])
@@ -61,7 +66,7 @@ def prepare():
 	global STATE
 	if STATE == WAIT_NAME or STATE == WAIT_START:
 		STATE = PLAYING
-		print('SERVER GO TO WAITNAME STATE')
+		e_print('SERVER GO TO WAITNAME STATE')
 		return 'OK'
 	else:
 		return 'errorPost'
@@ -87,22 +92,24 @@ def endgame():
 				print('SERVER GO TO WAITSTART STATE')
 			return 'game stop'
 		else:
+			e_print('WRONG GADGETS DATA RECIEVED')
 			return 'wrong gdata'
-	elif curr_id == -1:
-		return 'first start game'
 	else:
+		e_print('GAME NOT RUNNING')
 		return 'wrong method'
 
 
 @app.route('/getname')
-def ready():
+def getname():
 	global curr_id
 	if STATE == WAIT_START:
 		with sqlite3.connect("play.db") as connect:
 			com = Command(curr_id, connect)
 			if len(com.name) > 0:
+				e_print('NAME SEND TO MASTEROPERATOR: {}'.format(com.name))
 				return com.name
 	elif STATE == WAIT_NAME:
+		e_print('SERVER WAIT NAME FROM SCORES')
 		return 'SERVER_WAIT_NAME'
 # ========================= VIEW ROUTES =========================
 
@@ -114,24 +121,29 @@ def setname():
 		cName = request.form['cname']
 		dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		if len(cName) > 0:
-			with sqlite3.connect("play.db") as connect:
-				c = connect.cursor()
-				res = c.execute("""SELECT * from Commands""").fetchall()
-				next_id = -1
-				if len(res) > 0:
-					for r in res:
-						if r[0] > next_id:
-							next_id = r[0]
-				next_id += 1
-				c.execute("""INSERT INTO Commands VALUES (?,?,?)""",[next_id,dt,cName])
-				connect.commit()
-				curr_id = next_id
-				STATE = WAIT_START
-				print('CMD NAME: {}, WAIT START FROM MASTER'.format(cName))
+			try:
+				with sqlite3.connect("play.db") as connect:
+					c = connect.cursor()
+					res = c.execute("""SELECT * from Commands""").fetchall()
+					next_id = -1
+					if len(res) > 0:
+						for r in res:
+							if r[0] > next_id:
+								next_id = r[0]
+					next_id += 1
+					c.execute("""INSERT INTO Commands VALUES (?,?,?)""",[next_id,dt,cName])
+					connect.commit()
+					curr_id = next_id
+					STATE = WAIT_START
+					print('CMD NAME: {}, WAIT START FROM MASTER'.format(cName))
+			except Exception as e:
+				e_print('SET_NAME_ERROR: ' + e)
 			return 'SERVER_SET_CMDNAME_OK'
 		else:
+			e_print('SERVER_SET_CMDNAME_ERROR')
 			return 'SERVER_SET_CMDNAME_ERROR'
 	else:
+		e_print('SERVER_ERROR_REQUEST')
 		return 'SERVER_ERROR_REQUEST'
 
 @app.route('/getscores', methods = ['GET'])
@@ -143,7 +155,7 @@ def getscores():
 			return command.to_string()
 
 @app.route('/getstate')
-def state():
+def getstate():
 	if STATE == WAIT_NAME:
 		return 'WAIT_NAME'
 	elif STATE == PLAYING:
